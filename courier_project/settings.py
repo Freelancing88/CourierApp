4bba6397b78cd79142f5e1c86d44f3f1b163554c
OracleMoney <workspaceoracle9@gmail.com>
2026-04-06 11:41:18 +0100

    Removed all static vercel configurations

--- courier_project/settings.py
+++ courier_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a=sb#ao_cob55b+7tj%hdbd)6zzuj@f8o29-eqw!y$d%pbvz6s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
